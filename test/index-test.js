150a9e46fdeabf93c5737f223332d6fcf45b9055
mitchell williams <mscottwilliams1994@gmail.com>
2019-05-12 22:10:09 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,21 +1,21 @@
 var index = require("../index.js")
 
 describe('what-is-a-test', () => {
-  describe('Name', () => {
+  describe('name', () => {
     it('returns "Susan"', () => {
       expect(index.name).toEqual('Susan')
     })
   })
 
 
-  describe('Height', () => {
+  describe('height', () => {
     it('is less than 40', () => {
       expect(index.height).toBeLessThan(40)
     })
   })
 
   describe('Message', () => {
-    it('gives the Name and Height', () => {
+    it('gives the name and height', () => {
       expect(index.message).toInclude(index.name)
       expect(index.message).toInclude(index.height)
     })
